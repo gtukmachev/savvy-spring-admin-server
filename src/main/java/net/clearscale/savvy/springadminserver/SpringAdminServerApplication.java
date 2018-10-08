0a6a1d650ecf8fd26aba19dd9c18a573b0f385a2
Grigory <grigory@clearscale.net>
2018-10-08 14:48:34 +0700

    AWS Parameter Store integration

--- src/main/java/net/clearscale/savvy/springadminserver/SpringAdminServerApplication.java
+++ src/main/java/net/clearscale/savvy/springadminserver/SpringAdminServerApplication.java
@@ -9,6 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringAdminServerApplication {
 
 	public static void main(String[] args) {
+		setupAwsParameterStoreIntegration();
+
 		SpringApplication.run(SpringAdminServerApplication.class, args);
 	}
+
+	private static void setupAwsParameterStoreIntegration() {
+		String env = System.getProperty("AWS_ENV", "");
+		if (env.length() > 0) {
+			env = "/" +env;
+		}
+		System.setProperty("psSpringProfiles","prod,production"); // activated for 'production' spring profile only
+		System.setProperty("psRoots", env + "/sba," + env + "/common"); // 2 root folders will be used for reading properties
+	}
+
+
 }
